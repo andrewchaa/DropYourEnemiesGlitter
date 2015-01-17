b32a14c61bc41b637b014d5e2f7e3119666c6938
Andy <andrew.chaa@yahoo.co.uk>
2015-01-17 22:34:51 +0000

    express, vash, and layout

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var port = process.env.PORT || 3100
+
+try {
+  require('./config')();
+} catch (exception) {
+  console.log("No config, it must be in production.");
+}
+
+app.set('view engine', 'vash');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser('drop'));
+
+app.use('/', express.static('./public'));
+app.use('/api', router);
+
+var controllers = require('./controllers');
+controllers(app);
+
+var server = app.listen(port, function () {
+	console.log('listening on port %d', server.address().port);
+});
