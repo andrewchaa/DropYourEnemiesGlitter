6ad985d39ce0f30c42a04ab6cf0dfa2f3b23b08f
andrewchaa <andrew.chaa@huddle.com>
2015-01-19 18:55:29 +0000

    added order model, submit button

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,25 @@
 module.exports = function (app) {
 
+  var order = require('../models/order');
+
 	app.get('/', function (req, res) {
 		res.render('index', {});
 	});
+
+  app.post('drop', function (req, res) {
+    
+    var order = new Order();
+    order.email = req.body.email;
+    order.address = req.body.address;
+    order.postCode = req.body.postCode;
+
+    order.add(function (err) {
+      if (err)
+        res.status(400).send(err);
+
+      res.redirect('/dropped/' + order.id);
+    })
+
+  })
 
 }
---
+++ models/order.js
@@ -0,0 +1,54 @@
+function Order (id, email, address, postCode) {
+  this.id = id || '';
+  this.email = email || '';
+  this.address = address || '';
+  this.postCode = postCode || '';
+}
+
+var azure = require('azure-storage');
+var uuid = require('node-uuid');
+
+var tableName = 'orders';
+var partitionKey = 'myOrders';
+var storageAccount = process.env.AZURE_STORAGE_ACCOUNT;
+var storageAccessKey = process.env.AZURE_STORAGE_ACCESS_KEY;
+
+var entityGen = azure.TableUtilities.entityGenerator;
+var tableService = azure.createTableService(storageAccount, storageAccessKey);
+var TableQuery = azure.TableQuery;
+
+tableService.createTableIfNotExists(tableName, function (error) {
+  if (error)
+    throw error;
+});
+
+function convertToOrderFrom(row) {
+  var order = {};
+  order.id = row.RowKey._;
+  order.email = row.email._;
+  order.address = row.address._;
+  order.postCode = row.postCode._;
+
+  return order;
+}
+
+Order.prototype.add = function (next) {
+  this.id = uuid();
+  var orderEntity = {
+    PartitionKey: entityGen.String(partitionKey),
+    RowKey: entityGen.String(this.id),
+    id: entityGen.String(this.id),
+    email: entityGen.String(this.email),
+    address: entityGen.String(this.address),
+    postCode: entityGen.String()
+  }
+
+  tableService.insertEntity(tableName, orderEntity, function (err) {
+    if (err)
+      next(err);
+
+    next(null);
+  });
+}
+
+module.exports = Order;
