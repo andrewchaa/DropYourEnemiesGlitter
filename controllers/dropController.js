116cf471670e8d97333e4cfbe63d622a965211a5
Andy <andrew.chaa@yahoo.co.uk>
2015-01-25 12:05:37 +0000

    call paypal.configure before every api call

--- controllers/dropController.js
+++ controllers/dropController.js
@@ -9,6 +9,7 @@ module.exports = function (app) {
   });
   var paypalConfig = {
     'host': process.env.paypal_host,
+    'mode': process.env.paypal_mode,
     'client_id': process.env.paypal_client_id,
     'client_secret': process.env.paypal_client_secret
   };
@@ -27,62 +28,75 @@ module.exports = function (app) {
 
   app.post('/drop', function (req, res) {
     
-    // paypal.configure(paypalConfig);
+    log.info('paypal configure');
+    paypal.configure(paypalConfig);
 
-    var create_payment_json = {
-      "intent": "sale",
-      "payer": {
-        "payment_method": "paypal"
-      },
-      "redirect_urls": {
-        "return_url": "http://localhost:3100/pay",
-        "cancel_url": "http://localhost:3100/cancelled"
-      },
-      "transactions": [{
-         "item_list": {
-            "items": [{
-                "name": "ship your friend glitter",
-                "sku": "ship your friend glitter",
-                "price": "4.99",
-                "currency": "GBP",
-                "quantity": 1
-            }]
-        },
-        "amount": {
-          "currency": "GBP",
-          "total": "4.99"
-        },
-        "description": "Ship your friend glitter for fun!"
-      }]
-    };
-
-    paypal.payment.create(create_payment_json, paypalConfig, function (err, response) {
-      if (err) {
+    log.info('paypal generate token');
+    paypal.generate_token(function (err, token) {
+      if (err)
         throw err;
-      } else {
-        log.info("response: %s", JSON.stringify(response));
 
-        var approvalLink = response.links.filter(function (link) {
-          return link.rel == 'approval_url';
-        })[0];
+      log.info('access_token: ', token);
+      res.cookie('access_token', token);
 
-        var order = new Order();
-        order.email = req.body.email;
-        order.name = req.body.name;
-        order.address = req.body.address;
-        order.postCode = req.body.postCode;
-        order.note = req.body.note;
-        order.paymentId = response.id;
+      var create_payment_json = {
+        "intent": "sale",
+        "payer": {
+          "payment_method": "paypal"
+        },
+        "redirect_urls": {
+          "return_url": "http://localhost:3100/pay",
+          "cancel_url": "http://localhost:3100/cancelled"
+        },
+        "transactions": [{
+           "item_list": {
+              "items": [{
+                  "name": "ship your enemies glitter",
+                  "sku": "ship your enemies glitter",
+                  "price": "4.99",
+                  "currency": "GBP",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+            "currency": "GBP",
+            "total": "4.99"
+          },
+          "description": "Ship your enemies glitter for fun!"
+        }]
+      };
+
+      paypal.payment.create(create_payment_json, { Authorization: token}, 
+        function (err, response) {
+        if (err) {
+          throw err;
+        } else {
+          log.info("response: %s", JSON.stringify(response));
+
+          var approvalLink = response.links.filter(function (link) {
+            return link.rel == 'approval_url';
+          })[0];
+
+          var order = new Order();
+          order.email = req.body.email;
+          order.name = req.body.name;
+          order.address = req.body.address;
+          order.postCode = req.body.postCode;
+          order.note = req.body.note;
+          order.paymentId = response.id;
 
 
-        order.add(function (err) {
-          if (err)
-            res.status(400).send(err);
+          order.add(function (err) {
+            if (err)
+              res.status(400).send(err);
 
-          res.redirect(approvalLink.href);
-        })
-      }
+            res.redirect(approvalLink.href);
+          })
+        }
+      })
+
     })
+
   });
 
   app.get('/pay', function (req, res) {
@@ -92,7 +106,8 @@ module.exports = function (app) {
 
     log.info('query: ', req.query);
 
-    paypal.payment.execute(paymentId, payer, paypalConfig, function (err, response) {
+    paypal.configure(paypalConfig);
+    paypal.payment.execute(paymentId, payer, function (err, response) {
       if (err) {
         log.error(err);
         throw err;
